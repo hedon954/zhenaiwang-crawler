32079a219d3e730cde8de184407ede85cb4f3805
Hedon954 <171725713@qq.com>
2021-05-22 01:15:39 +0800

    add gitignore.txt

--- zhenai/parser/citylist.go
+++ zhenai/parser/citylist.go
@@ -7,15 +7,15 @@ import (
 )
 
 /**
- 珍爱网的城市列表解析器
- */
+珍爱网的城市列表解析器
+*/
 
 //<a href="http://www.zhenai.com/zhenghun/aba" data-v-1573aa7c>阿坝</a>
 const cityListRegex string = `<a href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]+)</a>`
 
 //解析城市列表
 //解析结果：城市名称 + 递归出来的请求
-func ParseCityList(content []byte) engine.ParseResult{
+func ParseCityList(content []byte) engine.ParseResult {
 
 	//提取出"url"和"city-name"
 	re := regexp.MustCompile(cityListRegex)
@@ -28,9 +28,9 @@ func ParseCityList(content []byte) engine.ParseResult{
 	matches := re.FindAllSubmatch(content, -1)
 	//封装数据到 ParseResult
 	result := engine.ParseResult{}
-	for _,m := range matches{
+	for _, m := range matches {
 		//城市的名字
-		result.Items = append(result.Items, "City " + string(m[2]))
+		//result.Items = append(result.Items, "City " + string(m[2]))
 		//后面需要做的请求
 		result.Requests = append(result.Requests,
 			engine.Request{
@@ -42,4 +42,3 @@ func ParseCityList(content []byte) engine.ParseResult{
 	}
 	return result
 }
-
